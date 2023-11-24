b7990a988d6490f67604c8ff806b4b1e91004f33
GideonAdelaja <adelajagideon@gmail.com>
2023-11-24 10:07:53 +0100

    updated

---
+++ ClassMembers/ClassMembers/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+class ChurchMember
+{
+    // Fields
+    private int memberId;
+    private string name;
+    private string emailAddress;
+    private DateTime joinDate;
+
+    // Properties
+    public int MemberId => memberId;
+    public string Name => name;
+    public string EmailAddress => emailAddress;
+    public DateTime JoinDate => joinDate;
+
+    // Enum for member status
+    enum MemberStatus
+    {
+        Active,
+        Inactive
+    }
+
+    // Delegate for member status change notification
+    delegate void ChangeMemberStatus(int memberId, MemberStatus newStatus);
+
+    // Interface for member management
+    interface IMemberManager
+    {
+        void AddMember(ChurchMember member);
+        void UpdateMemberInfo(ChurchMember member);
+        void RemoveMember(int memberId);
+        void ChangeMemberStatus(int memberId, MemberStatus newStatus);
+        bool IsMemberActive(int memberId);
+    }
+
+    // Methods
+
+    public void UpdateEmailAddress(string newEmailAddress)
+    {
+        emailAddress = newEmailAddress;
+        Console.WriteLine($"{name}'s email address has been updated to {emailAddress}");
+    }
+    public void Donate(double amount)
+    {
+        Console.WriteLine($"{name} donated {amount}Naira to the church.");
+    }
+
+    // Constructor
+    public ChurchMember(int memberId, string name, string emailAddress, DateTime joinDate)
+    {
+        this.memberId = memberId;
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.joinDate = joinDate;
+    }
+
+    // Destructor
+    ~ChurchMember()
+    {
+        Console.WriteLine($"{name}'s record has been released");
+    }
+
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a new church member
+        ChurchMember newMember = new ChurchMember(1234, "Emmanuel Temi", "temiemmanuel@gmail.com", DateTime.Now);
+
+        Console.WriteLine($"{newMember.Name} is a new member of our church \nHis Email Address is {newMember.EmailAddress} \nHe joined {newMember.JoinDate}");
+
+        // Update member email address
+        newMember.UpdateEmailAddress("temiemma@gmail.com");
+
+        // Make a donation
+        newMember.Donate(1500000.00);
+
+        //Show member details
+        Console.WriteLine($"The Latest Record of {newMember.Name} are:");
+        Console.WriteLine($"\tMember ID: {newMember.MemberId}");
+        Console.WriteLine($"\tEmail Address: {newMember.EmailAddress}");
+        Console.WriteLine($"\tJoin Date: {newMember.JoinDate}");
+
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -39,56 +39,85 @@ namespace ConsoleApp2
     {
         public static void Main(string[] args)
         {
-            // Create a User object
-        User user = new User(1, "Gideon Adelaja", "adelajagideon@gmail.com");
+            /*
+                // Create a User object
+            User user = new User(1, "Gideon Adelaja", "adelajagideon@gmail.com");
 
-        // Create Payment objects associated with the User
-        List<Payment> payments = new List<Payment>()
-        {
-            new Payment(101, user.UserId, 125000.00m, "House Hold Item Purchase", DateTime.Now),
-            new Payment(102, user.UserId, 250000.00m, "Dining Sets", DateTime.Now.AddDays(2))
-        };
-        Console.WriteLine("(1) A program defining two classes; User and Payment.\r\nUser class is related to Payment by UserId. in C#\r");
-        Console.WriteLine("------------------------\n");
-
-        // Display user information and associated payments
-        Console.WriteLine("USER INFORMATION: ");
-        Console.WriteLine("User ID: {0}", user.UserId);
-        Console.WriteLine("Name: {0}", user.Name);
-        Console.WriteLine("Email: {0}", user.Email);
-
-        Console.WriteLine("\nAssociated Payments: ");
-        foreach (Payment payment in payments)
-        {
-            Console.WriteLine("Payment ID: {0}", payment.PaymentId);
-            Console.WriteLine("Amount: {0} Naira", payment.Amount);
-            Console.WriteLine("Description: {0}", payment.Description);
-            Console.WriteLine("Payment Date: {0}", payment.PaymentDate);
-            Console.WriteLine();
-        }
-
-            //Question 2: Area of a triangle
-
-            // Declare variables and then initialize to zero.
-            double num1 = 0; double num2 = 0;
-
-            // Display title of calculator app.
-            Console.WriteLine("(2) A program that finds the area of a triangle in C#\r");
+            // Create Payment objects associated with the User
+            List<Payment> payments = new List<Payment>()
+            {
+                new Payment(101, user.UserId, 125000.00m, "House Hold Item Purchase", DateTime.Now),
+                new Payment(102, user.UserId, 250000.00m, "Dining Sets", DateTime.Now.AddDays(2))
+            };
+            Console.WriteLine("(1) A program defining two classes; User and Payment.\r\nUser class is related to Payment by UserId. in C#\r");
             Console.WriteLine("------------------------\n");
 
-            // Ask the user to enter the base.
-            Console.WriteLine("Enter the triangle's base, and then press Enter");
-            num1 = Convert.ToDouble(Console.ReadLine());
+            // Display user information and associated payments
+            Console.WriteLine("USER INFORMATION: ");
+            Console.WriteLine("User ID: {0}", user.UserId);
+            Console.WriteLine("Name: {0}", user.Name);
+            Console.WriteLine("Email: {0}", user.Email);
 
-            // Ask the user to enter the height.
-            Console.WriteLine("Enter the triangle's height, and then press Enter");
-            num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nAssociated Payments: ");
+            foreach (Payment payment in payments)
+            {
+                Console.WriteLine("Payment ID: {0}", payment.PaymentId);
+                Console.WriteLine("Amount: {0} Naira", payment.Amount);
+                Console.WriteLine("Description: {0}", payment.Description);
+                Console.WriteLine("Payment Date: {0}", payment.PaymentDate);
+                Console.WriteLine();
+            }
 
-            Console.WriteLine($"The area of a triangle with base {num1} and height {num2} is " + 0.5 * (num1 * num2));
+                //Question 2: Area of a triangle
 
-            // Wait for the user to respond before closing.
-            Console.Write("Press any key to close the Calculator console app...");
-            Console.ReadKey();
+                // Declare variables and then initialize to zero.
+                double num1 = 0; double num2 = 0;
+
+                // Display title of calculator app.
+                Console.WriteLine("(2) A program that finds the area of a triangle in C#\r");
+                Console.WriteLine("------------------------\n");
+
+                // Ask the user to enter the base.
+                Console.WriteLine("Enter the triangle's base, and then press Enter");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+                // Ask the user to enter the height.
+                Console.WriteLine("Enter the triangle's height, and then press Enter");
+                num2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"The area of a triangle with base {num1} and height {num2} is " + 0.5 * (num1 * num2));
+
+                // Wait for the user to respond before closing.
+                Console.Write("Press any key to close the Calculator console app...");
+                Console.ReadKey();
+
+                */
+
+            //CLASS PRACTICE
+
+            var studentAge = new int[6] { 1, 2, 3, 4, 5, 6, };
+            for(int counter = 0; counter < studentAge.Length; counter++)
+            {
+                Console.WriteLine(studentAge[counter]);
+            }
+            foreach(int counter2 in studentAge)
+            {
+                Console.WriteLine(studentAge);
+            }
+
+            //lIST
+            //syntax List is aKeyword: List<datatype> varname = new List<datatype>();
+            List<int> studentAge2 = new List<int>();
+            studentAge2.Add(23);
+            studentAge2.Add(45);
+            //Console.WriteLine(studentAge2[1];
+            for (int i = 0; i < studentAge2.Count; i++)
+            {
+                Console.WriteLine(studentAge2[i]);
+            }
+
+            //new user
+
 
 
         }
